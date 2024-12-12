c5ba230944a0e5404fc91a990ce88f9ce13a258c
lss0815 <js815002@gmail.com>
2024-12-12 14:40:39 +0900

    feat: implement decorator classes and main function

--- 3.decorator/starbuzz.cpp
+++ 3.decorator/starbuzz.cpp
@@ -11,25 +11,45 @@ class Coffee {
   double cost_{0};
 };
 
+class CoffeeTopping : public Coffee {
+ public:
+  explicit CoffeeTopping(std::shared_ptr<Coffee> base_coffee)
+      : base_coffee_(base_coffee) {}
+  std::string GetDescription() override {
+    return base_coffee_->GetDescription() + ", " + topping_name_ + ": " +
+           std::to_string(topping_cost_);
+  }
+  double GetCost() override { return base_coffee_->GetCost() + topping_cost_; }
+
+ protected:
+  std::shared_ptr<Coffee> base_coffee_;
+  std::string topping_name_;
+  double topping_cost_;
+};
+
 class Espresso : public Coffee {
  public:
   Espresso() { cost_ = 100.0; }
 
   std::string GetDescription() override {
-    return "Espresso: " + std::to_string(cost_) + ", ";
+    return "Espresso: " + std::to_string(cost_);
   }
 
   double GetCost() override { return cost_; }
 };
 
-class CoffeeTopping : public Coffee {
+class Whipping : public CoffeeTopping {
  public:
-  CoffeeTopping(std::shared_ptr<Coffee> coffee) {}
+  Whipping(std::shared_ptr<Coffee> base_coffee) : CoffeeTopping(base_coffee) {
+    topping_name_ = "whipping";
+    topping_cost_ = 10.0;
+  }
 };
 
 int main() {
-  Espresso order;
-  std::cout << order.GetDescription() << std::endl;
-  std::cout << "Total cost: " << order.GetCost() << std::endl;
+  std::shared_ptr<Coffee> order = std::make_shared<Whipping>(
+      std::make_shared<Whipping>(std::make_shared<Espresso>()));
+  std::cout << order->GetDescription() << std::endl;
+  std::cout << "Total cost: " << order->GetCost() << std::endl;
   return 0;
 }
